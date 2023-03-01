ec85acc70e48c6eac1bcb9358be13d837c58d85c
Brian Bassett <brianbassett@brians-mbp.mynetworksettings.com>
2023-03-01 07:53:20 -0500

    updated navbar, footer and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   constructor() {}
+
+  books = [1, 2, 3, 4, 5];
+  bookInfo = [
+    "Ender's Game is a brilliant introduction to Science Fiction in my opinion. The plot is captivating, the characters are interesting and who doesn't love aliens.",
+    'Foundations is really cool, you should read it.',
+    "There's been so much said about Game of Thrones both for the books and TV series. I won't bore you anymore.",
+    'Funny Space Book. Haha',
+    'This book is precious. My precious...',
+  ];
+
+  highlightedBook: number = 0;
+  selectedBook: number = 0;
+
+  updateHighlightedBook(i: number) {
+    if (this.highlightedBook == 0) {
+      this.highlightedBook = i;
+      return;
+    }
+    if (this.highlightedBook != 0 && i == this.highlightedBook) {
+      this.selectedBook = i;
+      return;
+    }
+    if (this.highlightedBook != 0 && i != this.highlightedBook) {
+      this.highlightedBook = i;
+      return;
+    }
+  }
+
+  resetBooks() {
+    this.highlightedBook = 0;
+    this.selectedBook = 0;
+  }
 }
+
+// when book is clicked, its highlighted
+// if book is highlighted and clicked again, its selected at which point, books should be dispersed
